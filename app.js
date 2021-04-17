0fa7817dcb9e60cf005999b8a9cdd8a0dadc88ea
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-17 13:57:38 +0200

    stage 1

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 class Counter extends React.Component {
-  state = {};
+  state = {
+    result: 1,
+    ratio: 2,
+  };
+
+  handleClick = () => {
+    this.setState((state) => ({
+      result: state.result * state.ratio,
+    }));
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
   render() {
     return (
       <div>
-        <p>Kalkulator</p>
+        <p>
+          Kalkulator mnoży przez dwa, jeśli suma jest mniejsza niż 1000. Po przekroczeniu 1000
+          kalkulator mnoży przez 0.5, aż osiągnie sumę mniejszą niż jeden. Wtedy ponownie zaczyna
+          mnożyć przez dwa.
+        </p>
+        <button onClick={this.handleClick}>{`Pomnóż przez ${this.state.ratio}`}</button>
+        <h1>Wynik: {this.state.result}</h1>
       </div>
     );
   }
